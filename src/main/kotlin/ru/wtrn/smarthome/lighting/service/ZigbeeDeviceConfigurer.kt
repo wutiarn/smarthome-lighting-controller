69ad9f4231c8a865aa45f373a63eebb9f65c66e4
Dmitry Romanov <me@wtrn.ru>
2021-12-12 23:37:35 +0300

    WIP: Cron jobs

--- src/main/kotlin/ru/wtrn/smarthome/lighting/service/ZigbeeDeviceConfigurer.kt
+++ src/main/kotlin/ru/wtrn/smarthome/lighting/service/ZigbeeDeviceConfigurer.kt
@@ -1,13 +1,16 @@
 package ru.wtrn.smarthome.lighting.service
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import mu.KotlinLogging
 import org.eclipse.paho.client.mqttv3.MqttClient
 import org.eclipse.paho.client.mqttv3.MqttMessage
 import org.springframework.stereotype.Service
 
 @Service
 class ZigbeeDeviceConfigurer(private val objectMapper: ObjectMapper, private val mqttClient: MqttClient) {
+    private val logger = KotlinLogging.logger {}
     fun changeDeviceState(deviceId: String, state: Any) {
+        logger.info { "Updating device $deviceId state: $state" }
         val messageBytes = objectMapper.writeValueAsBytes(state);
         val message = MqttMessage(messageBytes).also {
             it.qos = 0
